beb6d898df6e59a62d23fc48a66f77648bbc1112
ByteNinja <pijayblog@gmail.com>
2026-01-19 09:14:15 +0700

    Add home.php for the home view

---
+++ app/view/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= htmlspecialchars($title) ?></title>
+</head>
+<body>
+    <h1><?= htmlspecialchars($message) ?></h1>
+    <p>Khumba Phase 1 berhasil.</p>
+</body>
+</html>
