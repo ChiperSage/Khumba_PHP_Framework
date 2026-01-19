4c4f672825a29a275b611f08c28c4cd62ea896bd
ByteNinja <pijayblog@gmail.com>
2026-01-19 09:10:20 +0700

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+// public/index.php
+
+define('BASE_PATH', dirname(__DIR__));
+
+require BASE_PATH . '/system/core/Router.php';
+require BASE_PATH . '/system/core/Controller.php';
+
+// simple autoload app
+spl_autoload_register(function ($class) {
+    $paths = [
+        BASE_PATH . '/app/controller/',
+        BASE_PATH . '/app/model/',
+        BASE_PATH . '/system/core/',
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $class . '.php';
+        if (file_exists($file)) {
+            require $file;
+            return;
+        }
+    }
+});
+
+// run router
+Router::run();
