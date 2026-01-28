c5f0533418e18d758564df9cbe19593c2c1f35c8
ByteNinja <pijayblog@gmail.com>
2026-01-28 15:44:03 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,55 @@
 <?php
 // public/index.php
-ExceptionHandler::register();
 
 define('BASE_PATH', dirname(__DIR__));
 
+/*
+ |-------------------------------------------------
+ | Security Headers
+ |-------------------------------------------------
+ */
+header('X-Frame-Options: SAMEORIGIN');
+header('X-Content-Type-Options: nosniff');
+header('X-XSS-Protection: 1; mode=block');
+
+/*
+ |-------------------------------------------------
+ | Session
+ |-------------------------------------------------
+ */
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+/*
+ |-------------------------------------------------
+ | Core Load (WAJIB URUTAN INI)
+ |-------------------------------------------------
+ */
+require BASE_PATH . '/system/core/ExceptionHandler.php';
 require BASE_PATH . '/system/core/Router.php';
 require BASE_PATH . '/system/core/Controller.php';
 require BASE_PATH . '/system/core/Helper.php';
 
+/*
+ |-------------------------------------------------
+ | Register Exception
+ |-------------------------------------------------
+ */
+ExceptionHandler::register();
+
+/*
+ |-------------------------------------------------
+ | CSRF Verify (POST only)
+ |-------------------------------------------------
+ */
 csrf_verify();
 
-// simple autoload app
+/*
+ |-------------------------------------------------
+ | Autoload App & Core
+ |-------------------------------------------------
+ */
 spl_autoload_register(function ($class) {
     $paths = [
         BASE_PATH . '/app/controller/',
@@ -27,5 +66,9 @@ spl_autoload_register(function ($class) {
     }
 });
 
-// run router
+/*
+ |-------------------------------------------------
+ | Run Application
+ |-------------------------------------------------
+ */
 Router::run();
