eb3639206b7db00e3319fe4ec85326ee22b1f46d
ByteNinja <pijayblog@gmail.com>
2026-01-28 15:32:54 +0700

    Add database configuration file

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+// config/database.php
+
+return [
+    'driver'   => 'mysql',      // mysql | pgsql | sqlite
+    'host'     => '127.0.0.1',
+    'database' => 'khumba_db',
+    'username' => 'root',
+    'password' => '',
+    'charset'  => 'utf8',
+];
